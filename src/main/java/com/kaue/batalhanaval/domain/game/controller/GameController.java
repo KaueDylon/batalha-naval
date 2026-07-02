75ef1021b4542b3d711b59cd7094609a27aa6be3
KaueDylon <kaue.dylon33@gmail.com>
2026-07-02 10:11:39 -0300

    refactor: ReApagado imports sem uso em GameController e rede métodos comentados anteriormente

--- src/main/java/com/kaue/batalhanaval/domain/game/controller/GameController.java
+++ src/main/java/com/kaue/batalhanaval/domain/game/controller/GameController.java
@@ -1,8 +1,4 @@
 package com.kaue.batalhanaval.domain.game.controller;
-
-import com.kaue.batalhanaval.domain.game.dto.AttackRequest;
-import com.kaue.batalhanaval.domain.game.dto.AttackResult;
-import com.kaue.batalhanaval.domain.game.dto.PlaceShipRequest;
 import com.kaue.batalhanaval.domain.game.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,22 +20,6 @@ public class GameController {
         String gameId = gameService.createGame(user.toString(), playerBid);
         return ResponseEntity.ok(gameId);
     }
-
-//    @PostMapping("/{id}/place")
-//    public ResponseEntity<Boolean> place(@PathVariable String id,
-//                                         @RequestBody PlaceShipRequest req,
-//                                         @AuthenticationPrincipal UUID user){
-//        boolean ok = gameService.placeShip(id, user.toString(), req);
-//        return ResponseEntity.ok(ok);
-//    }
-//
-//    @PostMapping("/{id}/attack")
-//    public ResponseEntity<AttackResult> attack(@PathVariable String id,
-//                                               @RequestBody AttackRequest req,
-//                                               @AuthenticationPrincipal UUID user){
-//        AttackResult result = gameService.attack(id, user.toString(), req.row(), req.col());
-//        return ResponseEntity.ok(result);
-//    }
 
     @GetMapping("/{id}/board/{targetId}")
     public ResponseEntity<int[][]> board(@PathVariable String id,
