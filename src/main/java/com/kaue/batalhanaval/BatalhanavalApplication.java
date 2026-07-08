da9d957301dd7e924b73f18910547bf4ca7f7102
KaueDylon <kaue.dylon33@gmail.com>
2026-07-08 09:57:09 -0300

    config: adiciona .env.example e vars de ambiente

--- src/main/java/com/kaue/batalhanaval/BatalhanavalApplication.java
+++ src/main/java/com/kaue/batalhanaval/BatalhanavalApplication.java
@@ -1,5 +1,6 @@
 package com.kaue.batalhanaval;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BatalhanavalApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BatalhanavalApplication.class, args);
 	}
 
